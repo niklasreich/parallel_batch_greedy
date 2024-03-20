3fff7a3295b0c1f496155a2cdab8748fdec5f21a
niklasreich <niklas.reich@hs-ruhrwest.de>
2024-03-20 14:00:38 +0100

    updated benchmark script

--- src/pymordemos/mpi_thermalblock_benchmark.sh
+++ src/pymordemos/mpi_thermalblock_benchmark.sh
@@ -1,24 +1,35 @@
 #!/bin/bash
-mpiexec -n 2 python -m thermalblock_script 3 2 9 0
-mpiexec -n 3 python -m thermalblock_script 3 2 9 0
-mpiexec -n 4 python -m thermalblock_script 3 2 9 0
-mpiexec -n 5 python -m thermalblock_script 3 2 9 0
-mpiexec -n 6 python -m thermalblock_script 3 2 9 0
-mpiexec -n 7 python -m thermalblock_script 3 2 9 0
-mpiexec -n 8 python -m thermalblock_script 3 2 9 0
-mpiexec -n 10 python -m thermalblock_script 3 2 9 0
-mpiexec -n 12 python -m thermalblock_script 3 2 9 0
-mpiexec -n 14 python -m thermalblock_script 3 2 9 0
-mpiexec -n 16 python -m thermalblock_script 3 2 9 0
-python -m thermalblock_script 3 2 9 1
-python -m thermalblock_script 3 2 9 2
-python -m thermalblock_script 3 2 9 3
-python -m thermalblock_script 3 2 9 4
-python -m thermalblock_script 3 2 9 5
-python -m thermalblock_script 3 2 9 6
-python -m thermalblock_script 3 2 9 7
-python -m thermalblock_script 3 2 9 8
-python -m thermalblock_script 3 2 9 10
-python -m thermalblock_script 3 2 9 12
-python -m thermalblock_script 3 2 9 14
-python -m thermalblock_script 3 2 9 16
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 1
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 2
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 3
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 4
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 5
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 6
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 8
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 10
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 12
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 14
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 16
+# mpiexec -n 2 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 3 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 4 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 5 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 6 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 7 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 8 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 10 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 12 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 14 python -m thermalblock_script 2 2 25 0
+# mpiexec -n 16 python -m thermalblock_script 2 2 25 0
+python -m thermalblock_script 2 2 25 1
+python -m thermalblock_script 2 2 25 2
+python -m thermalblock_script 2 2 25 3
+python -m thermalblock_script 2 2 25 4
+python -m thermalblock_script 2 2 25 5
+python -m thermalblock_script 2 2 25 6
+python -m thermalblock_script 2 2 25 7
+python -m thermalblock_script 2 2 25 8
+python -m thermalblock_script 2 2 25 10
+python -m thermalblock_script 2 2 25 12
+python -m thermalblock_script 2 2 25 14
+python -m thermalblock_script 2 2 25 16
