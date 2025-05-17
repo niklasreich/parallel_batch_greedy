95920930d052386ffe5b4381b74fb56dda992a3c
niklasreich <niklas.reich@hs-ruhrwest.de>
2025-05-17 13:25:05 +0200

    plotting results

--- src/pymordemos/thermalblock_plot_final.py
+++ src/pymordemos/thermalblock_plot_final.py
@@ -1,25 +1,26 @@
 import numpy as np
 from pymor.core.pickle import load
 import matplotlib.pyplot as plt
-from os.path import isfile
+from os.path import isfile, join
+from os import listdir
 
+file_string = 'thermalblock_2x2'
 
 max_batchsize = 30
 plot_batch = [1, 2, 4, 8, 16]
 
-procs = 30
+procs = 8
 
 t_evaluate = []
 t_extend = []
 t_greedy = []
 t_offline = []
-t_postprocess = []
 t_reduce = []
 t_solve = []
 t_online = []
 
-batchsizes = []
-batchsizes_str = []
+lambda_tol = []
+lambda_str = []
 
 num_ext = []
 num_iter = []
@@ -27,15 +28,34 @@ num_pp = []
 
 ax00_y_max = 0
 
+files = [f for f in listdir('src/pymordemos') if isfile(join('src/pymordemos', f))]
+files = [f for f in files if file_string in f and 'lambda' in f]
+files = [f for f in files if f'N{procs}' in f or 'B1' in f]
+
 fig, ax = plt.subplots(2,2)
 
-for bs in range(1, max_batchsize+1):
+for f in files:
 
-    file_string = f'src/pymordemos/thermalblock_2x2_N{procs}_BS{bs}.pkl'
+    with open('src/pymordemos/'+f, 'rb') as f_:
+        results = load(f_)
 
-    if isfile(file_string):
-        with open(file_string, 'rb') as f:
-            results = load(f)
+    if 'B1' in f:
+        rev_t_online = timings['online']
+        rev_t_offline = timings['offline']
+        rev_t_eveluate = timings['evaluate']
+        rev_t_extend = timings['extend']
+        rev_t_reduce = timings['reduce']
+        rev_t_solve = timings['solve']
+        rev_t_greedy = timings['greedy']
+
+        rev_num_iter = results['num_iterations']
+        rev_num_ext = results['num_extensions']
+
+        ax[0,0].semilogy(results['max_rel_errors'][0],label='classical')
+        if len(results['max_rel_errors'][0]) > ax00_y_max:
+            ax00_y_max = len(results['max_rel_errors'][0])
+
+    else:
 
         timings = results['timings']
 
@@ -43,41 +63,66 @@ for bs in range(1, max_batchsize+1):
         t_extend.append(timings['extend'])
         t_greedy.append(timings['greedy'])
         t_offline.append(timings['offline'])
-        t_postprocess.append(timings['postprocess'])
         t_reduce.append(timings['reduce'])
         t_solve.append(timings['solve'])
         t_online.append(timings['online'])
 
-        batchsizes_str.append((str(bs)))
-        batchsizes.append(bs)
-
         num_ext.append(results['num_extensions'])
         num_iter.append(results['num_iterations'])
-        num_pp.append(results['num_extensions'] - len(results['max_errs_pp']))
+
+        lambda_val = results['settings']['lambda']
+        lambda_tol.append(lambda_val)
+        lambda_str.append(f"{lambda_val}")
 
         ### Subplot 00 (Upper left)
-        if bs in plot_batch:
-            ax[0,0].semilogy(results['max_rel_errors'][0],label=f'$b={bs}$')
-            if len(results['max_rel_errors'][0]) > ax00_y_max:
-                ax00_y_max = len(results['max_rel_errors'][0])
+        ax[0,0].semilogy(results['max_rel_errors'][0],':',label=f'$\lambda={lambda_val}$')
+        if len(results['max_rel_errors'][0]) > ax00_y_max:
+            ax00_y_max = len(results['max_rel_errors'][0])
+
+
+t_evaluate.append(rev_t_eveluate)
+t_extend.append(rev_t_extend)
+t_offline.append(rev_t_offline)
+t_reduce.append(rev_t_reduce)
+t_solve.append(rev_t_solve)
+t_online.append(rev_t_online)
+t_greedy.append(rev_t_greedy)
+num_ext.append(rev_num_ext)
+num_iter.append(rev_num_iter)
+lambda_tol.append(2.) #Put at the end after sorting
+lambda_str.append('CWG')
+
 
 t_evaluate = np.array(t_evaluate)
 t_extend = np.array(t_extend)
 t_greedy = np.array(t_greedy)
 t_offline = np.array(t_offline)
-t_postprocess = np.array(t_postprocess)
 t_reduce = np.array(t_reduce)
 t_solve = np.array(t_solve)
 t_online = np.array(t_online)
+lambda_tol = np.array(lambda_tol)
+lambda_str = np.array(lambda_str)
 
-t_other = t_offline - t_evaluate - t_extend - t_postprocess - t_reduce - t_solve
+sort_ind = np.argsort(lambda_tol)
 
-t_sequential = {'solve': t_solve, 'evaluate': t_evaluate, 'extend': t_extend, 'reduce': t_reduce, 'postprocess': t_postprocess, 'other': t_other}
+t_evaluate = t_evaluate[sort_ind]
+t_extend = t_extend[sort_ind]
+t_greedy = t_greedy[sort_ind]
+t_offline = t_offline[sort_ind]
+t_reduce = t_reduce[sort_ind]
+t_solve = t_solve[sort_ind]
+t_online = t_online[sort_ind]
+lambda_tol = lambda_tol[sort_ind]
+lambda_str = lambda_str[sort_ind]
+
+t_other = t_offline - t_evaluate - t_extend - t_reduce - t_solve
+
+t_sequential = {'solve': t_solve, 'evaluate': t_evaluate, 'extend': t_extend, 'reduce': t_reduce, 'other': t_other}
 
 fig.suptitle(f'2x2 Thermalblock')
 
 ### Subplot 00 (Upper left)
-ax[0,0].legend(loc="upper right")
+ax[0,0].legend(loc="lower left")
 ax[0,0].set_ylabel('err in $H^1_0$ semi norm')
 ax[0,0].set_xlabel('basis size n')
 ax[0,0].grid(axis='y')
@@ -85,35 +130,34 @@ ax[0,0].set_axisbelow(True)
 ax[0,0].plot([0,ax00_y_max],[1e-5,1e-5],'k--')
 
 ### Subplot 01 (Upper right)
-bottom = np.zeros(len(batchsizes_str))
+bottom = np.zeros(len(lambda_tol))
 width = 0.5
 for title, time in t_sequential.items():
-    p = ax[0,1].bar(batchsizes_str, time, width, label=title, bottom=bottom)
+    p = ax[0,1].bar(lambda_str, time, width, label=title, bottom=bottom)
     bottom += time
-ax[0,1].legend(loc="upper right")
+ax[0,1].legend(loc="upper left")
 ax[0,1].set_ylabel('offline time [s]')
-ax[0,1].set_xlabel('batch size b')
+ax[0,1].set_xlabel('$\lambda$')
 ax[0,1].grid(axis='y')
 ax[0,1].set_axisbelow(True)
 
 ### Subplot 10 (Lower left)
-ax[1,0].plot([1,np.max(batchsizes)],[num_ext[0],num_ext[0]],'k--')
-ax[1,0].plot(batchsizes, num_ext, 'o:', label='Basis size without postprocessing')
-ax[1,0].plot(batchsizes, num_pp, 'o:', label='Basis size with postprocessing')
-ax[1,0].plot(batchsizes, num_iter, 'o:', label='greedy iterations')
+ax[1,0].semilogx([np.min(lambda_tol),1],[rev_num_iter,rev_num_iter],'k--')
+ax[1,0].semilogx(lambda_tol[:-1], num_ext[:-1], 'o:', label='Basis size')
+ax[1,0].semilogx(lambda_tol[:-1], num_iter[:-1], 'o:', label='Greedy Iterations')
 ax[1,0].grid(axis='y')
 ax[1,0].set_axisbelow(True)
-ax[1,0].set_xlabel('batch size b')
+ax[1,0].set_xlabel('$\lambda$')
 ax[1,0].legend(loc=0)
 
 ### Subplot 10 (Lower right)
-t_online_n = t_online/t_online[0]
-t_offline_n = t_offline/t_offline[0]
-ax[1,1].plot([1,np.max(batchsizes)],[1,1],'k--')
-ax[1,1].plot(batchsizes, t_online_n, 'o:', label='Norm. online time')
-ax[1,1].plot(batchsizes, t_offline_n, 'o:', label='Norm. offline time')
+t_online_n = t_online/rev_t_online
+t_offline_n = t_offline/rev_t_offline
+ax[1,1].semilogx([np.min(lambda_tol),1],[1,1],'k--')
+ax[1,1].semilogx(lambda_tol[:-1], t_online_n[:-1], 'o:', label='Norm. online time')
+ax[1,1].semilogx(lambda_tol[:-1], t_offline_n[:-1], 'o:', label='Norm. offline time')
 ax[1,1].grid(axis='y')
 ax[1,1].set_axisbelow(True)
-ax[1,1].set_xlabel('batch size b')
+ax[1,1].set_xlabel('$\lambda$')
 ax[1,1].legend(loc=0)
 plt.show()
