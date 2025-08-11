bb577bc5f1b25b40bc670920a4e1592bb744bc59
niklas.reich <niklas.reich@hs-ruhrwest.de>
2025-08-11 08:32:30 +0200

    updated benchmark file

--- src/pymordemos/mpi_thermalblock_benchmark.sh
+++ src/pymordemos/mpi_thermalblock_benchmark.sh
@@ -3,17 +3,30 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 export NUMEXPR_NUM_THREADS=1
 # mpiexec -n 30 python -m thermalblock_script 3 2 9 1
-mpiexec -n 8 python -m thermalblock_script 2 2 15 1 1
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 1
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.8
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.6
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.4
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.2
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.1
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.08
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.06
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.04
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.02
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.01
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.005
-mpiexec -n 8 python -m thermalblock_script 2 2 15 8 0.001
+# mpiexec -n 30 python -m thermalblock_script 2 2 15 1 1
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 1
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.3
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.1
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.03
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.01
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.003
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.001
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.0003
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.0001
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.00003
+mpiexec -n 30 python -m thermalblock_script 2 2 25 30 0.00001
+# mpiexec -n 30 python -m thermalblock_script 2 2 15 30 0.02
+# mpiexec -n 30 python -m thermalblock_script 2 2 15 30 0.01
+# mpiexec -n 30 python -m thermalblock_script 2 2 15 30 0.005
+# mpiexec -n 30 python -m thermalblock_script 2 2 15 30 0.001
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 1
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.3
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.1
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.03
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.01
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.003
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.001
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.0003
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.0001
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.00003
+mpiexec -n 30 python -m thermalblock_script 2 3 10 30 0.00001
